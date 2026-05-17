105eba3a1b453d2a89f66b7d435f93b07af4e7c5
Tomas Torday <tomas.torday@gmail.com>
2026-05-17 10:28:25 +0200

    Add custom command to remove bold chars in md

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -25,15 +25,38 @@ k.set("v", "j", "gj", { silent = true })
 k.set("v", "k", "gk", { silent = true })
 
 -- Toggle conceallevel between 0 and 3
-vim.keymap.set('n', '<leader>tc', function()
-  local current = vim.opt_local.conceallevel:get()
-  if current == 0 then
-    vim.opt_local.conceallevel = 3
-  else
-    vim.opt_local.conceallevel = 0
-  end
+vim.keymap.set("n", "<leader>tc", function()
+    local current = vim.opt_local.conceallevel:get()
+    if current == 0 then
+        vim.opt_local.conceallevel = 3
+    else
+        vim.opt_local.conceallevel = 0
+    end
 end, { buffer = true, desc = "Toggle Conceal" })
 
+-- Custom Commands {{{1
+
+-- Create a buffer-local user command :RemoveBold
+-- The '0' argument targets the current buffer only
+vim.api.nvim_buf_create_user_command(0, "RemoveBold", function(opts)
+    -- Save the current cursor position to prevent it from jumping
+    local save_cursor = vim.fn.getpos(".")
+    -- Determine the range (defaults to whole file '%' if no range is given)
+    local range = "%"
+    if opts.range > 0 then
+        range = string.format("%d,%d", opts.line1, opts.line2)
+    end
+    -- Run the substitution string safely using Lua's raw string syntax
+    -- the `{-}` means non-greedy search
+    vim.cmd(string.format([[%ss/\*\*\(.\{-}\)\*\*/\1/ge]], range))
+    -- Restore the cursor position
+    vim.fn.setpos(".", save_cursor)
+end, {
+    range = true, -- Allows the command to accept a range (e.g., visual selection)
+    desc = "Remove bold markdown formatting from the file or selection",
+})
+
+-- Colors & Highlighting {{{1
 
 -- use `:Inspect` command to get the highlight names
 -- of the items under cursor in the buffer
@@ -50,20 +73,20 @@ local comment = "#5c6773" -- Muted grey
 local green = "#95e6cb" -- Aqua/Green
 
 local header_configs = {
-	{ fg = "#f07178", bg = "#253340" }, -- H1: Coral
-	{ fg = "#ffad66", bg = "#253340" }, -- H2: Orange
-	{ fg = "#ffcc66", bg = "#253340" }, -- H3: Gold
-	{ fg = "#95e6cb", bg = "#253340" }, -- H4: Aqua
-	{ fg = "#36a3d9", bg = "#253340" }, -- H5: Blue
-	{ fg = "#5c6773", bg = "#253340" }, -- H6: Greyish Blue
+    { fg = "#f07178", bg = "#253340" }, -- H1: Coral
+    { fg = "#ffad66", bg = "#253340" }, -- H2: Orange
+    { fg = "#ffcc66", bg = "#253340" }, -- H3: Gold
+    { fg = "#95e6cb", bg = "#253340" }, -- H4: Aqua
+    { fg = "#36a3d9", bg = "#253340" }, -- H5: Blue
+    { fg = "#5c6773", bg = "#253340" }, -- H6: Greyish Blue
 }
 
 for i = 1, 6 do
-	a.nvim_set_hl(0, "@markup.heading." .. i .. ".markdown", {
-		fg = header_configs[i].fg,
-		bg = header_configs[i].bg,
-		bold = true,
-	})
+    a.nvim_set_hl(0, "@markup.heading." .. i .. ".markdown", {
+        fg = header_configs[i].fg,
+        bg = header_configs[i].bg,
+        bold = true,
+    })
 end
 
 -- 2. Styling
@@ -87,7 +110,11 @@ a.nvim_set_hl(0, "@punctuation.special.markdown", { fg = "#475266" })
 
 -- 6. Links and Images
 a.nvim_set_hl(0, "@markup.link.markdown_inline", { fg = accent })
-a.nvim_set_hl(0, "@markup.link.label.markdown_inline", { fg = accent, underline = true })
+a.nvim_set_hl(
+    0,
+    "@markup.link.label.markdown_inline",
+    { fg = accent, underline = true }
+)
 
 -- 7. Todo Checks
 a.nvim_set_hl(0, "@markup.list.unchecked.markdown", { fg = green })
--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -2,64 +2,65 @@ local a = vim.api
 
 -- Master table for groups {{{1
 local groups = {
-	yank = a.nvim_create_augroup("YankHighlight", { clear = true }),
-	labels = a.nvim_create_augroup("CommentLabels", { clear = true }),
-	position = a.nvim_create_augroup("LastPosition", { clear = true }),
-	clear_msg = a.nvim_create_augroup("ClearMsgGroup", { clear = true }),
+    yank = a.nvim_create_augroup("YankHighlight", { clear = true }),
+    labels = a.nvim_create_augroup("CommentLabels", { clear = true }),
+    position = a.nvim_create_augroup("LastPosition", { clear = true }),
+    clear_msg = a.nvim_create_augroup("ClearMsgGroup", { clear = true }),
 }
 
 -- MiniFiles customize windows {{{1
 vim.api.nvim_create_autocmd("User", {
-	pattern = "MiniFilesWindowOpen",
-	callback = function(args)
-		local win_id = args.data.win_id
-		-- Customize window-local settings
-		-- vim.wo[win_id].winblend = 50
-		local config = vim.api.nvim_win_get_config(win_id)
-		config.border, config.title_pos = "rounded", "right"
-		vim.api.nvim_win_set_config(win_id, config)
-	end,
+    pattern = "MiniFilesWindowOpen",
+    callback = function(args)
+        local win_id = args.data.win_id
+        -- Customize window-local settings
+        -- vim.wo[win_id].winblend = 50
+        local config = vim.api.nvim_win_get_config(win_id)
+        config.border, config.title_pos = "rounded", "right"
+        vim.api.nvim_win_set_config(win_id, config)
+    end,
 })
 
 -- Highlight on yank {{{1
 a.nvim_create_autocmd("TextYankPost", {
-	group = groups.yank,
-	callback = function()
-		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 150 })
-	end,
+    group = groups.yank,
+    callback = function()
+        vim.highlight.on_yank({ higroup = "IncSearch", timeout = 150 })
+    end,
 })
 
 -- Note, fixme and todo labels {{{1
 a.nvim_create_autocmd({ "BufWinEnter", "WinEnter" }, {
-	group = groups.labels,
-	callback = function()
-		vim.fn.matchadd("TodoLabel", "TODO:")
-		vim.fn.matchadd("FixmeLabel", "FIXME:")
-		vim.fn.matchadd("NoteLabel", "NOTE:")
-	end,
+    group = groups.labels,
+    callback = function()
+        vim.fn.matchadd("TodoLabel", "TODO:")
+        vim.fn.matchadd("FixmeLabel", "FIXME:")
+        vim.fn.matchadd("NoteLabel", "NOTE:")
+    end,
 })
 
 -- Return to last edit position {{{1
 a.nvim_create_autocmd("BufReadPost", {
-	group = groups.position,
-	desc = "Jump to last edit position",
-	callback = function()
-		local mark = a.nvim_buf_get_mark(0, '"')
-		local lcount = a.nvim_buf_line_count(0)
-		if mark[1] > 0 and mark[1] <= lcount then
-			pcall(a.nvim_win_set_cursor, 0, mark)
-		end
-	end,
+    group = groups.position,
+    desc = "Jump to last edit position",
+    callback = function()
+        local mark = a.nvim_buf_get_mark(0, '"')
+        local lcount = a.nvim_buf_line_count(0)
+        if mark[1] > 0 and mark[1] <= lcount then
+            pcall(a.nvim_win_set_cursor, 0, mark)
+        end
+    end,
 })
 
 -- Auto clear the command line {{{1
 a.nvim_create_autocmd({ "BufWritePost", "TextYankPost" }, {
-	group = groups.clear_msg,
-	callback = function()
-		vim.defer_fn(function()
-			if vim.fn.getcmdtype() == "" then
-				a.nvim_echo({ { "" } }, false, {})
-			end
-		end, 5000)
-	end,
+    group = groups.clear_msg,
+    callback = function()
+        vim.defer_fn(function()
+            if vim.fn.getcmdtype() == "" then
+                a.nvim_echo({ { "" } }, false, {})
+            end
+        end, 5000)
+    end,
 })
+
