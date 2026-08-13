13dcc9b762397dbba8cb7f2592880abe68624100
Tomas Torday <tomas.torday@gmail.com>
2026-08-13 11:37:11 +0200

    Add kdl formatter to commands.lua Format custom command

--- lua/core/commands.lua
+++ lua/core/commands.lua
@@ -48,6 +48,7 @@ local formatters = {
     css = "prettier --write %",
     html = "prettier --write %",
     fish = "fish_indent -w %",
+    kdl = "kdlfmt format %",
     lua = "stylua --indent-type=Spaces --indent-width=4 --quote-style=AutoPreferDouble --column-width=80 %",
     python = "ruff format %",
     javascript = "prettier --write %",
