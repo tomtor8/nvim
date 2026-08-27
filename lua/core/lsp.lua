5b7fa6ea9df1701f8a2df9390fe92c2da45f6ebe
Tomas Torday <tomas.torday@gmail.com>
2026-08-27 18:41:23 +0200

    fix: change lua_lsp settings in lsp.lua

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -43,42 +43,42 @@ vim.lsp.enable("basedpyright")
 
 -- Lua {{{1
 -- 1. Define the config in a simple table
-local lua_ls_config = {
-    cmd = { "lua-language-server" },
-    name = "lua_ls",
-    -- root_dir = vim.fs.root(0, { ".git", "init.lua", ".luarc.json" }) or (vim.uv or vim.loop).cwd(),
-    root_markers = { ".luarc.json", ".git", ".stylua.toml" },
-    settings = {
-        Lua = {
-            diagnostics = { globals = { "vim" } },
-            workspace = {
-                library = {
-                    vim.env.VIMRUNTIME,
-                    "${3rd}/luv/library",
-                },
-                checkThirdParty = false,
-            },
-        },
-    },
-}
+-- local lua_ls_config = {
+--     cmd = { "lua-language-server" },
+--     name = "lua_ls",
+--     -- root_dir = vim.fs.root(0, { ".git", "init.lua", ".luarc.json" }) or (vim.uv or vim.loop).cwd(),
+--     root_markers = { ".luarc.json", ".git", ".stylua.toml" },
+--     settings = {
+--         Lua = {
+--             diagnostics = { globals = { "vim" } },
+--             workspace = {
+--                 library = {
+--                     vim.env.VIMRUNTIME,
+--                     "${3rd}/luv/library",
+--                 },
+--                 checkThirdParty = false,
+--             },
+--         },
+--     },
+-- }
 
 -- 2. Use a "BufEnter" autocommand. This is the most aggressive trigger.
 -- It runs every time you enter a buffer.
-vim.api.nvim_create_autocmd("BufEnter", {
-    pattern = "*.lua",
-    callback = function()
-        -- Directly start the client for the current buffer
-        vim.lsp.start(lua_ls_config)
-    end,
-})
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--     pattern = "*.lua",
+--     callback = function()
+--         -- Directly start the client for the current buffer
+--         vim.lsp.start(lua_ls_config)
+--     end,
+-- })
 
--- vim.lsp.config['lua_ls'] = {
---   cmd = { 'lua-language-server' },
---   filetypes = { 'lua' },
---   root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
--- }
---
--- vim.lsp.enable('lua_ls')
+vim.lsp.config['lua_ls'] = {
+  cmd = { 'lua-language-server' },
+  filetypes = { 'lua' },
+  root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
+}
+
+vim.lsp.enable('lua_ls')
 
 -- Rust {{{1
 -- on Arch after sudo pacman -S rustup
